4f7941615403f420c8b9d7fb3f525c6daf9f41d1
Koldo Santisteban Lopez <ksantisteban@gmail.com>
2016-03-16 08:22:18 +0100

    Added files via upload

---
+++ DhtSender/dht.ino
@@ -0,0 +1,43 @@
+//DHT22
+#include <SoftwareSerial.h>
+#include "DHT.h"
+
+#define DHTPIN 4 
+
+const int SensorPin = A0; 
+int Value = 0; 
+int SensorValue = 0; 
+SoftwareSerial XBee(8, 9); // RX, TX
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+  XBee.begin(9600);
+  //Serial.println("Start!!");
+  delay(2000);
+}
+
+void loop() {
+  // Read the sensors first:
+
+   float t = dht.readTemperature();
+
+   if (isnan(t)) {
+    //Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  else
+  {
+  XBee.print("T");
+  XBee.println(t);
+  //Serial.print ("T");
+  Serial.println(t);
+  
+  delay(15000);
+  }
+
+}
+
